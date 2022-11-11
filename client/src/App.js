bde67b5cd97cab41d8907d1d45814d74fa83cb84
mws75 <mwspencer75@gmail.com>
2022-11-11 12:05:29 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
+
+function App() {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{width: "90%", margin:"auto auto", textAlign:"center"}}>
+      <h1>Home Page</h1>
+      <Button variant="outline-dark" style={{width:"100%"}}  onClick={() => navigate("create")}>NextPlease</Button>
+    </div>
+  );
+}
+
+export default App;
